f0bb6c0c3d8dc02eddf3f5527361711b353f8fd8
Saud Shaban <awesomeinfinity8@outlook.com>
2019-12-21 22:32:39 +0300

    completed reportThread and reportReply

--- controllers/BoardController.js
+++ controllers/BoardController.js
@@ -1,6 +1,6 @@
+let ObjectId = require('mongoose').Types.ObjectId;
+
 const Board = require("../models/Board").model;
-const Thread = require("../models/Thread").model;
-const ReplyModel = require("../models/Reply").model;
 
 function postThread(req, res) {
   let created = new Date();
@@ -65,28 +65,83 @@ function postReply(req, res) {
 }
 
 function getBoard(req, res) {
+  Board.aggregate(
+    [
+      {
+        $match: {
+          name: req.params.board
+        }
+      },
+      {
+        $unwind: {
+          path: "$threads"
+        }
+      },
+      {
+        $unwind: {
+          path: "$threads.replies"
+        }
+      },
+      {
+        $match: {
+          "threads.reported": false,
+          "threads.replies.reported": false
+        }
+      },
+      {
+        $sort: {
+          "threads.bumped_on": 1,
+          "threads.replies.created_on": 1
+        }
+      },
+      {
+        $group: {
+          _id: "$threads._id",
+          text: {
+            $first: "$threads.text"
+          },
+          created_on: {
+            $first: "$threads.created_on"
+          },
+          bumped_on: {
+            $first: "$threads.bumped_on"
+          },
+          replies: {
+            $push: {
+              text: "$threads.replies.text",
+              _id: "$threads.replies._id",
+              created_on: "$threads.replies.created_on"
+            }
+          }
+        }
+      },
+      {
+        $limit: 10
+      }
+    ],
+    function boardsCallback(err, docs) {
+      if (err) {
+        res.send(err);
+      } else {
+        docs.map(doc => {
+          doc.replies.splice(3);
+        });
+        res.json(docs);
+      }
+    }
+  );
+}
+
+function getThread(req, res) {
   Board.aggregate([
     {
       '$match': {
-        'name': 'general'
+        'name': req.params.board, 
+        'threads._id': new ObjectId(req.query.thread_id)
       }
     }, {
       '$unwind': {
         'path': '$threads'
-      }
-    }, {
-      '$unwind': {
-        'path': '$threads.replies'
-      }
-    }, {
-      '$match': {
-        'threads.reported': false, 
-        'threads.replies.reported': false
-      }
-    }, {
-      '$sort': {
-        'threads.bumped_on': 1, 
-        'threads.replies.created_on': 1
       }
     }, {
       '$group': {
@@ -94,54 +149,113 @@ function getBoard(req, res) {
         'text': {
           '$first': '$threads.text'
         }, 
-        'created_on': {
-          '$first': '$threads.created_on'
-        }, 
         'bumped_on': {
           '$first': '$threads.bumped_on'
         }, 
+        'created_on': {
+          '$first': '$threads.created_on'
+        }, 
         'replies': {
-          '$push': {
-            'text': '$threads.replies.text', 
-            '_id': '$threads.replies._id', 
-            'created_on': '$threads.replies.created_on'
-          }
+          '$first': '$threads.replies'
         }
       }
-    }, {
-      '$limit': 10
     }
-  ], function boardsCallback(err, docs){
+  ], function getThreadCallback(err, doc){
     if(err){
       res.send(err);
     }
     else {
-      docs.map((doc) => {
-        doc.replies.splice(3);
-      });
-      res.json(docs);
+      res.json(doc[0]);
     }
   });
 }
 
-function getThread(req, res) {
-  res.send("Not yet implemented");
-}
-
 function deleteThread(req, res) {
-  res.send("Not yet implemented");
+  Board.update({
+    name: req.params.board
+  }, {
+    "$pull": {
+      "_id": new ObjectId(req.body.thread_id),
+      "delete_password": req.body.delete_password
+    }
+  }, function deleteThreadCallback(err, raw){
+    if(err){
+      res.send("incorrect password");
+    }
+    else {
+      res.send("success");
+    }
+  });
 }
 
 function deleteReply(req, res) {
-  res.send("Not yet implemented");
+  Board.update({
+    name: req.params.board
+  }, {
+    "$pull": {
+      "threads":{
+        "_id": new ObjectId(req.body.thread_id),
+        "replies._id": new ObjectId(req.body.reply_id),
+        "replies.delete_password": req.body.delete_password
+    }
+  }
+  }, function deleteThreadCallback(err, raw){
+    if(err){
+      res.send("incorrect password");
+    }
+    else {
+      res.send("success");
+    }
+  });
 }
 
 function reportThread(req, res) {
-  res.send("Not yet implemented");
+  Board.updateOne(
+    {
+        'name': req.params.board, 
+        'threads._id': new ObjectId(req.body.thread_id)
+    }, {
+      '$set': {
+        'threads.$.reported': true
+      }
+    }
+  , function reportThreadCallback(err, doc){
+    if(err){
+      res.send(err);
+    }
+    else {
+      res.send('success');
+    }
+  });
 }
 
 function reportReply(req, res) {
-  res.send("Not yet implemented");
+  Board.updateOne(
+    {
+        'name': req.params.board,
+    }, {
+      '$set': {
+        'threads.$[threadElement].replies.$[replyElement].reported': true
+      }
+    },
+    {
+      arrayFilters: [
+        {
+          "threadElement._id": new ObjectId(req.body.thread_id)
+        },
+        {
+          "replyElement._id": new ObjectId(req.body.reply_id)
+        }
+      ]
+    }
+  , function reportReplyCallback(err, doc){
+    if(err){
+      res.send(err);
+    }
+    else {
+      res.send('success');
+    }
+  });
 }
 
 module.exports = {
